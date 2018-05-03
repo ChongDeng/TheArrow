f649b31e92e0c43f043bfbcbd68f94f9936c38b5
chong <fqyyang@163.com>
2018-05-03 08:31:52 -0700

    added article permanant link

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,3 +1,4 @@
+from flask_pagedown.fields import PageDownField
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, BooleanField, SelectField
 from wtforms.validators import DataRequired, Length, Regexp, ValidationError, Email
@@ -47,5 +48,5 @@ class EditProfileAdminForm(FlaskForm):
             raise ValidationError('Username already in use.')
 
 class PostForm(FlaskForm):
-    body = TextAreaField("What's on your mind?", validators=[DataRequired()])
+    body = PageDownField("What's on your mind?", validators=[DataRequired()])
     submit = SubmitField('Submit')
