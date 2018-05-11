653a6e4b56dd3b04429539b072e256e2ecf3ebf7
chong <fqyyang@163.com>
2018-05-10 21:17:18 -0700

    http auth method 4: added token

--- auth_token.py
+++ auth_token.py
@@ -2,13 +2,16 @@ import os
 from flask import Flask, g, request, jsonify, url_for, make_response
 from flask_restful import abort
 
+from itsdangerous import (TimedJSONWebSignatureSerializer
+                          as Serializer, BadSignature, SignatureExpired)
+
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] =\
     'sqlite:///' + os.path.join(basedir, 'auth_token_data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+app.config['SECRET_KEY'] = 'heibazahei'
 
 # extensions
 from flask_sqlalchemy import SQLAlchemy
@@ -27,6 +30,22 @@ class User(db.Model):
 
     def verify_password(self, password):
         return pwd_context.verify(password, self.password_hash)
+
+    def generate_auth_token(self, expiration=600):
+        s = Serializer(app.config['SECRET_KEY'], expires_in=expiration)
+        return s.dumps({'id': self.id})
+
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except SignatureExpired:
+            return None  # valid token, but expired
+        except BadSignature:
+            return None  # invalid token
+        user = User.query.get(data['id'])
+        return user
 
     def __repr__(self):
         return '<SCUT User %r>' % self.username
@@ -69,16 +88,27 @@ def get_resource():
 
 
 @auth.verify_password
-def verify_password(username, password):
-    user = User.query.filter_by(username=username).first()
-    if not user or not user.verify_password(password):
-        return False
+def verify_password(username_or_token, password):
+    # first: try to authenticate by token
+    user = User.verify_auth_token(username_or_token)
+    if not user:
+        # second: try to authenticate with username/password
+        user = User.query.filter_by(username=username_or_token).first()
+        if not user or not user.verify_password(password):
+            return False
     g.user = user
     return True
 
 @auth.error_handler
 def unauthorized():
     return make_response(jsonify({'error': 'You have no right to access'}), 401)
+
+
+@app.route('/api/token')
+@auth.login_required
+def get_auth_token():
+    token = g.user.generate_auth_token()
+    return jsonify({ 'token': token.decode('ascii') })
 
 # curl -s -D - http://localhost:5000 -H 'Content-Type: application/json'
 @app.route('/')
